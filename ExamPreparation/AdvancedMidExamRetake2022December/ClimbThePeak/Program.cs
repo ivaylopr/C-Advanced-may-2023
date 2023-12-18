5dcf5fcd53c7ce9380956ffe6cd53a40572eff6e
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-18 09:00:15 +0200

    Adding solution of exam preparation

---
+++ ExamPreparation/AdvancedMidExamRetake2022December/ClimbThePeak/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+
+Dictionary<string, int> peeksInfo = new Dictionary<string, int>()
+{
+    { "Vihren",80 },
+    { "Kutelo",90 },
+    { "Banski Suhodol",100 },
+    { "Polezhan",60 },
+    { "Kamenitza",70 }
+
+};
+Queue<string> peeksNames=new(new List<string>() { "Vihren", "Kutelo", "Banski Suhodol", "Polezhan", "Kamenitza" });
+List<string> conqueredNames = new();
+Stack<int> foodPortions = new(Console.ReadLine()
+                                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse));
+Queue<int> staminaQuantity = new(Console.ReadLine()
+                                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse));
+while (foodPortions.Any() && staminaQuantity.Any() && peeksNames.Any())
+{
+    int foodPortion = foodPortions.Pop();
+    int dayliStamina = staminaQuantity.Dequeue();
+    int peekDificultly = peeksInfo[peeksNames.Peek()];
+    if (foodPortion+dayliStamina>=peekDificultly)
+    {
+        conqueredNames.Add(peeksNames.Dequeue());
+    }
+}
+if (peeksNames.Any())
+{
+    Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+}
+else
+{
+    Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+}
+if (conqueredNames.Count>0)
+{
+    Console.WriteLine("Conquered peaks:");
+    foreach (var item in conqueredNames)
+    {
+        Console.WriteLine(item);
+    }
+}
+
---
+++ ExamPreparation/AdvancedMidExamRetake2022December/NavyBattle/Program.cs
@@ -0,0 +1,71 @@
+﻿int size = int.Parse(Console.ReadLine());
+char[,] battleField = new char[size, size];
+int submarineRow = 0;
+int submarineCol = 0;
+
+for (int row = 0; row < size; row++)
+{
+    string input = Console.ReadLine();
+    for (int col = 0; col < size; col++)
+    {
+        battleField[row, col] = input[col];
+        if (battleField[row, col] == 'S')
+        {
+            submarineRow = row;
+            submarineCol = col;
+        }
+    }
+
+}
+
+int hitsTaken = 0;
+int enemiesDown = 3;
+while (true)
+{
+    battleField[submarineRow, submarineCol] = '-';
+    string command = Console.ReadLine();
+    if (command == "up")
+    {
+        submarineRow--;
+    }
+    else if (command == "down")
+    {
+        submarineRow++;
+    }
+    else if (command == "left")
+    {
+        submarineCol--;
+    }
+    else if (command == "right")
+    {
+        submarineCol++;
+    }
+    if (battleField[submarineRow, submarineCol] == 'C')
+    {
+        enemiesDown--;
+    }
+    else if (battleField[submarineRow, submarineCol] == '*')
+    {
+        hitsTaken++;
+    }
+    if (enemiesDown == 0)
+    {
+        Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+
+        break;
+    }
+    else if (hitsTaken == 3)
+    {
+        Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{submarineRow}, {submarineRow}]!");
+        break;
+    }
+}
+battleField[submarineRow, submarineCol] = 'S';
+for (int row = 0; row < size; row++)
+{
+    for (int col = 0; col < size; col++)
+    {
+        Console.Write(battleField[row, col]);
+    }
+    Console.WriteLine();
+}
