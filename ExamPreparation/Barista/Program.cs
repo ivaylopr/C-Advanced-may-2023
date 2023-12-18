4e98a66ca5a653b81041b34427a8bce5b1904361
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-18 09:02:36 +0200

    add preparation

---
+++ ExamPreparation/Barista/Program.cs
@@ -0,0 +1,94 @@
+﻿Queue<int> coffeQuantity = new(Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+Stack<int> milkQuantity = new (Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()); 
+Dictionary<string,int> drinks=new Dictionary<string, int>()
+{
+    {"Cortado",0 },
+    {"Espresso",0 },
+    {"Capuccino",0 },
+    {"Americano",0 },
+    {"Latte",0 }
+
+};
+int sum = 0;
+while (coffeQuantity.Count>0 && milkQuantity.Count>0)
+{
+    int currentCoffe= coffeQuantity.Dequeue();
+    int currentMilk = milkQuantity.Pop();
+    sum = currentCoffe + currentMilk;
+    if (sum==50)
+    {
+        drinks["Cortado"]++;
+    }
+    else if (sum==75)
+    {
+        
+        drinks["Espresso"]++;
+
+    }
+    else if (sum == 100)
+    {
+        
+        drinks["Capuccino"]++;
+
+    }
+    else if (sum == 150)
+    {
+         drinks["Americano"]++;
+
+    }
+    else if (sum == 200)
+    {
+        drinks["Latte"]++;
+    }
+    else
+    {
+        currentMilk -= 5;
+        milkQuantity.Push(currentMilk);
+    }
+
+}
+if (coffeQuantity.Count==0 && milkQuantity.Count==0)
+{
+    Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+    Console.WriteLine("Coffee left: none");
+    Console.WriteLine("Milk left: none");
+}
+
+else
+{
+    Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+    if (coffeQuantity.Count>0)
+    {
+        Console.Write("Coffee left: ");
+        foreach (var item in coffeQuantity)
+        {
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("Coffee left: none");
+    }
+    if (milkQuantity.Count > 0)
+    {
+        Console.Write("Coffee left: ");
+        foreach (var item in milkQuantity)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("Milk left: none");
+    }
+    
+}
+foreach (var drink in drinks.OrderBy(d => d.Value).ThenByDescending(d => d.Key))
+{
+    if (drink.Value>0)
+    {
+        Console.WriteLine($"{drink.Key}: {drink.Value}");
+    }
+}
---
+++ ExamPreparation/BlindMan’sBuff/Program.cs
@@ -0,0 +1,140 @@
+﻿int[] size =Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int n=size[0];
+int m = size[1];
+char[,] field = new char[n,m];
+int playerRow = 0;
+int playerCol = 0;
+
+
+for (int row = 0; row < n; row++)
+{
+    string[] inputField = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+    for (int col = 0; col < m; col++)
+	{
+		field[row, col] = char.Parse(inputField[col]);
+		if (field[row,col]=='B')
+		{
+			playerCol = col;
+			playerRow = row;
+		}
+	}
+}
+string command=String.Empty;
+int movesCnt = 0;
+int tuchedCnt = 0;
+while ((command=Console.ReadLine())!="Finish")
+{
+	if (command=="up")
+	{
+		playerRow--;
+        if (IsIn(playerRow, playerCol, field))
+        {
+            if (field[playerRow, playerCol] == 'O')
+            {
+                playerRow++;
+                continue;
+            }
+            if (field[playerRow, playerCol] == 'P')
+            {
+                field[playerRow, playerCol] = '-';
+                tuchedCnt++;
+            }
+            movesCnt++;
+        }
+        else
+        {
+            playerRow++;
+
+        }
+        
+
+        
+	}
+	else if (command=="down" )
+	{
+        playerRow++;
+        if (IsIn(playerRow, playerCol, field))
+        {
+           
+            if (field[playerRow, playerCol] == 'O')
+            {
+                playerRow--;
+                continue;
+            }
+            if (field[playerRow, playerCol] == 'P')
+            {
+                field[playerRow, playerCol] = '-';
+                tuchedCnt++;
+            }
+            movesCnt++;
+        }
+        else
+        {
+            playerRow--;
+        }
+		
+    }
+    else if (command == "left")
+    {
+		playerCol--;
+        if (IsIn(playerRow, playerCol, field))
+        {
+            if (field[playerRow, playerCol] == 'O')
+            {
+                playerCol++;
+                continue;
+            }
+            if (field[playerRow, playerCol] == 'P')
+            {
+                field[playerRow, playerCol] = '-';
+                tuchedCnt++;
+            }
+            movesCnt++;
+        }
+        else
+        {
+            playerCol++;
+        }
+        
+    }
+    else if (command == "right")
+    {
+		playerCol++;
+        if (IsIn(playerRow, playerCol, field))
+        {
+            if (field[playerRow, playerCol] == 'O')
+            {
+                playerCol--;
+                continue;
+            }
+            if (field[playerRow, playerCol] == 'P')
+            {
+                field[playerRow, playerCol] = '-';
+                tuchedCnt++;
+            }
+            movesCnt++;
+        }
+        else
+        {
+            playerCol -= 1;
+        }
+        
+    }
+
+    if (tuchedCnt==3)
+    {
+        break;
+    }
+}
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {tuchedCnt} Moves made: {movesCnt}");
+static bool IsIn(int row,int col, char[,]field)
+{
+	bool isIn = true;
+    if (row<0 || col<0 || row>field.GetLength(0)-1 || col>field.GetLength(1)-1)
+	{
+		isIn=false;
+	}
+	return isIn;
+}
