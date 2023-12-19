13ac93b4c1b00a051d23db72369bebe5cafa20f3
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-19 16:59:30 +0200

    add solution of problems about Defining class

---
+++ Defining Class/CarManufacturer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+using System.Xml.Schema;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car("BMW", "X3", 2005, 100, 2);
+           
+            car.Drive(10);
+            Console.WriteLine(car.WhoAmI());
+
+        }
+    }
+}
