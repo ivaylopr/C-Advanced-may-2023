b2a173fa9f0ae6f434f338f3118555708cbb7be0
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2023-12-19 17:35:43 +0200

    add solution of problems about Generics

---
+++ Generics/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Generics/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+            Console.WriteLine($"{box.ToString()}");
+        }
+
+    }
+}
